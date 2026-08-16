d9b3a20c2cb52357fa372c7b57116f6bbf3a3a8e
preeti sharma <97779553+preetisharma12@users.noreply.github.com>
2026-08-16 23:17:35 +0200

    Add example conversation data

---
+++ src/data/conversation.js
@@ -0,0 +1,19 @@
+const conversation = [
+  {
+    trigger: "I have an optimization problem.",
+    reply: "What would you like to optimize?",
+  },
+
+  {
+    trigger: "Transport orders in an automated warehouse.",
+    reply:
+      "That sounds like a scheduling problem. Is that correct?",
+  },
+
+  {
+    trigger: "Yes",
+    reply: "Please describe the problem in more detail.",
+  },
+];
+
+export default conversation;
